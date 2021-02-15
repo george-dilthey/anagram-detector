a89ef2cbbcae7b97ebb360864a5dcf0960a2cf58
George Dilthey <george.dilthey@gmail.com>
2021-02-15 15:01:32 -0500

    commit tree e437ab7d4e5248ea3a3711dea4e1c3c6306f1072

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(text)
+        word_array = word.split("").sort
+        return_array = []
+        text.each {|i| 
+            text_array = i.split("").sort
+            if word_array == text_array
+                return_array.push(i)
+            end
+        }
+        return return_array
+    end
+end
